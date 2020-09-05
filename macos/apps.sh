a8f2417e12b21813b6905740796571d39ad93f07
David Torras <davidtorrasceron@gmail.com>
2020-09-05 10:46:52 +0200

    first commit

---
+++ macos/apps.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+echo 'start macos/apps.sh'
+
+brew cask install iterm2
+brew cask install sublime-text
+brew cask install slack
+brew cask install spotify
+brew cask install firefox
+brew cask install brave-browser
+brew cask install google-chrome
+brew cask install 1password
+brew cask install telegram
+brew cask install alfred
+brew cask install rectangle
